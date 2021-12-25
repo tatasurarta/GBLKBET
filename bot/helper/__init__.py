ebd9c9b995c2a58c8f8c620caf6d73e06bfe0e6f
tatasurarta <91466068+tatasurarta@users.noreply.github.com>
2021-12-25 17:04:27 +0700

    Delete TelegraphPoster

--- bot/helper/__init__.py
+++ bot/helper/__init__.py
@@ -6,7 +6,6 @@ import heroku3
 from functools import wraps
 from pyrogram.types import Message
 from typing import Tuple
-from html_telegraph_poster import TelegraphPoster
 from bot import HEROKU_API_KEY, HEROKU_APP_NAME
 
 # Implement by https://github.com/jusidama18
@@ -73,19 +72,6 @@ def fetch_heroku_git_url(api_key, app_name):
     return heroku_app.git_url.replace("https://", "https://api:" + api_key + "@")
 
 HEROKU_URL = fetch_heroku_git_url(HEROKU_API_KEY, HEROKU_APP_NAME)
-
-def post_to_telegraph(a_title: str, content: str) -> str:
-    """ Create a Telegram Post using HTML Content """
-    post_client = TelegraphPoster(use_api=True)
-    auth_name = "Rumah Awan 2"
-    post_client.create_api_token(auth_name)
-    post_page = post_client.post(
-        title=a_title,
-        author=auth_name,
-        author_url="https://t.me/awanmirror2bot",
-        text=content,
-    )
-    return post_page["url"]
 
 
 async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
